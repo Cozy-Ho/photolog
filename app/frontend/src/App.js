a1b28ba07073be13e09331ce2272ff5b1c6d4f8d
cozy-ho <kj2693119@gmail.com>
2021-05-07 11:30:40 +0900

    [Add] default Modal

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import Mapbox from "./components/Map.js";
+import Modal from "./components/Modals/Modal.js";
 
 function App() {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const openModal = () => {
+        setModalOpen(true);
+    };
+    const closeModal = () => {
+        setModalOpen(false);
+    };
     return (
         <div className="App">
+            <div>
+                <button onClick={openModal}> == </button>
+                <Modal open={modalOpen} close={closeModal} header="Modal heading">
+                    test message!
+                </Modal>
+            </div>
             <div>
                 <Mapbox></Mapbox>
             </div>
